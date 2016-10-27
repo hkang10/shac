2c3b599d5705bdcc96252135421463d400833310
Audrey Kang <hkang10@gmail.com>
2016-10-26 19:02:10 -0500

    add associations still working on them

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,5 +1,5 @@
 class Friendship < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :friend, class_name: "User"
+  belongs_to :initiated_friend, class_name: "User"
+  belongs_to :received_friend, class_name: "User",, foreign_key: :friend_id
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,20 @@
 class User < ActiveRecord::Base
-has_many :groupings, foreign_key: :member_id
-has_many :groups, through: :groupings
-has_many :created_groups, class_name: "Group", foreign_key: :admin_id
+  has_many :groupings, foreign_key: :member_id
+  has_many :groups, through: :groupings
+  has_many :created_groups, class_name: "Group", foreign_key: :admin_id
 
-has_many :hostings, class_name: "Crashing", foreign_key: :host_id
-has_many :crashings, foreign_key: :guest_id
-has_many :guests, through: :hostings
-has_many :hosts, through: :crashings
+  has_many :hostings, class_name: "Crashing", foreign_key: :host_id
+  has_many :crashings, foreign_key: :guest_id
+  has_many :guests, through: :hostings, source: :host
+  has_many :hosts, through: :crashings, source: :guest
 
-has_many :friends
-has_many :messages
+  has_many :initiated_friendships, class_name: "Friendship"
+  has_many :received_friendships, class_name: "Friendship", foreign_key: :friend_id
+  has_many :messages
+
+  # def friends
+  #   self.initiated_friendships.received_friend +
+  #   self.received_friendships.initiated_friend
+  # end
 
 end
